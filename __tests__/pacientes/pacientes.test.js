a6314793f37180502cf413749fcfd9c3163cce89
Alan <aafloress>
2022-02-21 15:29:01 -0600

    Sol Act-Pru

--- __tests__/pacientes/pacientes.test.js
+++ __tests__/pacientes/pacientes.test.js
@@ -3,7 +3,7 @@ const supertest = require("supertest");
 describe ('Test suite de api v1 pacientes endpoint', ()=> {
   it("GET /api/v1/pacientes/", async ()=> {
     await supertest(app).get('/api/v1/pacientes')
-      .set({ apitoken:'53ca569b-81e1-47a8-93a6-8c9b54524724'})
+      .set({ apitoken:'d427f8ec-495f-4482-a182-e4025197e37e'})
       .expect(200);
   });
 });
